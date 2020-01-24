c56370bce5cad45fc66ee894ef05b1a3905b1021
alejo9604 <juaalvarezme@gmail.com>
2020-01-23 23:14:18 -0500

    Set first animation version. Update and clean roulette prefab. Add post procesing

---
+++ Assets/Scr/Editor/SceneViewAlign.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SceneViewAlign : MonoBehaviour
+{
+    [MenuItem("Editor/Set Scene View %h")]
+    static void SetSceneViewAlign()
+    {
+        Transform cam = Camera.main.transform;
+        var sceneView = SceneView.lastActiveSceneView;
+        if (sceneView != null) {
+            sceneView.AlignViewToObject(cam);
+            sceneView.orthographic = false;
+        }
+
+    }
+}
